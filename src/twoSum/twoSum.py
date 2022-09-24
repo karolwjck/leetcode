959a8f985e5377c5f1fd6fe9bc94fda299df615c
karol wojcik <karolwojcik29@gmail.com>
2022-09-24 14:23:21 +0100

    twoSum Python

---
+++ src/twoSum/twoSum.py
@@ -0,0 +1,13 @@
+from ast import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevHashMap = {} # Mapping the vlaue to index of that value
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevHashMap:
+                return [prevHashMap[diff], i]
+            # If solution not found we update the prevHashMap
+            prevHashMap[n] = i
+        return
